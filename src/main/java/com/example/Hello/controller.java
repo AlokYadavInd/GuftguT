d8aa2193c3fc583d69df736d8d7834edcc8233cf
AlokYadavInd <alok.yada.ind@gmail.com>
2022-07-09 16:04:29 +0530

    Changed The Port Num

--- src/main/java/com/example/Hello/controller.java
+++ src/main/java/com/example/Hello/controller.java
@@ -1,15 +1,19 @@
 package com.example.Hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-	
+
+	@Value("${server.port}")
+	private String valueFromFile;
 	@GetMapping("/hello")
 	public String hello()
 	{
-		return "<H1>I am running inside Docker ContsinerHello Sring Boot</H1>";
+		System.out.printf("Port"+valueFromFile);
+		return "<H1>I am running inside Docker Contsiner Hello Sring Boot</H1>";
 	}
 
 }
